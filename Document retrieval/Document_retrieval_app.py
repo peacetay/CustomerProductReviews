69718a2ad692bae6dc90d63898625cca8560ed35
yenyunhsuan <126640641+yenyunhsuan@users.noreply.github.com>
2023-10-20 21:01:27 +0800

    Update Document_retrieval_app.py

--- Document retrieval/Document_retrieval_app.py
+++ Document retrieval/Document_retrieval_app.py
@@ -1,48 +1,22 @@
 import streamlit as st
 import re
 import pandas as pd
-import tensorflow_hub as hub
-from nltk.corpus import stopwords
-from openai.embeddings_utils import cosine_similarity
+from gensim import similarities
+import nltk
+import gensim
+from gensim import corpora
+from gensim import models
+
+# nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
 ###############################################################################################################################################
 # Import data
 
 def load_reviews():
-    df = pd.read_excel(r'C:\Users\Tina\OneDrive - Singapore Management University\SMU\Term 3\Text analytics\Project\amazon_review JOIN.xlsx')
+    df = pd.read_csv(r'C:\Users\Tina\OneDrive - Singapore Management University\SMU\Term 3\Text analytics\Project\Document retrieval\document_retrieval_update.csv')
     return df
 
 df = load_reviews()
-
-###############################################################################################################################################
-# Functions and cleaning
-
-def clean_text(text):
-    text = re.sub(r'[^a-zA-Z\s]', '', str(text))
-    text = text.lower()
-    return text
-
-df['Cleaned Reviews'] = df['Review Content'].apply(clean_text)
-
-def remove_stopwords(text):
-    stop_words = set(stopwords.words('english'))
-    words = text.split()
-    filtered_words = [word for word in words if word.lower() not in stop_words]
-    return ' '.join(filtered_words)
-
-def brand(model):
-    match = re.search(r'^(HP|Epson|Canon)', str(model), re.IGNORECASE)
-    if match:
-        return match.group(0)
-
-def embed(text, model):
-    embeddings = model([text])
-    return embeddings.numpy()[0]
-
-model = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
-
-df['Final Reviews'] = df['Cleaned Reviews'].apply(remove_stopwords)
-df['Brand'] = df['Review Model'].apply(brand)
-df['Embed_sentence'] = df['Final Reviews'].apply(lambda x: embed(x, model))
 
 ###############################################################################################################################################
 # Header and filter
@@ -55,6 +29,7 @@ st.write("Select a topic to find related reviews.")
 brand = st.selectbox("Select Brand", Brand)
 
 labels = [
+    '',
     'Setup',
     'Connectivity',
     'Customer Support',
@@ -68,34 +43,59 @@ labels = [
     'Control Panel'
 ]
 
-Rating = [1,2,3,4,5]
+Rating = [1, 2, 3, 4, 5]
 key_words = st.selectbox("Select Topic", labels)
 if key_words == "":
-    input_keywords = st.text_input('Type Keywords')
+    with st.expander('Or type your own keywords'):
+        input_keywords = st.text_input('Type Keywords')
 else:
-    input_keywords = ""
+    with st.expander('Or type your own keywords'):
+        input_keywords = ""
 
-with st.expander('Or type your own key words'):
-    input_keywords = st.text_input('Type Keywords')
+# with st.expander('Or type your own keywords'):
+#     input_keywords = st.text_input('Type Keywords')
 rating = st.selectbox("Select Rating", Rating)
 apply_button = st.button('Apply')
 
+def stem_text(text):
+    stemmer = PorterStemmer()
+    words = text.split()  # Split the text into words
+    stemmed_words = [stemmer.stem(word) for word in words]  # Apply stemming
+    return ' '.join(stemmed_words)
 
-def embed_label(text, model):
-    embeddings = model([text])
-    return embeddings.numpy()[0]
+df['review stem'] = df['Full review'].apply(stem_text)
+stemmed_texts = df['review stem'].str.split()  # Split the stemmed text into lists of words
+sgDictionary = corpora.Dictionary(stemmed_texts)
+sgVectors = [sgDictionary.doc2bow(doc) for doc in stemmed_texts]
+sgIndex = similarities.SparseMatrixSimilarity(sgVectors, len(sgDictionary))
+sgTFIDF = models.TfidfModel(sgVectors)
+sgVectorsWithTFIDF = [sgTFIDF[vec] for vec in sgVectors]
+sgIndexWithTFIDF = similarities.SparseMatrixSimilarity(sgVectorsWithTFIDF, len(sgDictionary))
+
+def query(text):
+    qList = text.split()  
+    qLower = [w.lower() for w in qList]
+    stemmer = PorterStemmer()
+    qStemmed = [stemmer.stem(w) for w in qLower]
+    qVector = sgDictionary.doc2bow(qStemmed)
+    qVectorTFIDF = sgTFIDF[qVector]
+    return qVectorTFIDF, qVector
 
 if apply_button:
-    input_text = key_words if key_words != "" else input_keywords
+    input_text = key_words if key_words != '' else input_keywords
 
     if input_text:
-        input_embedding_vector = embed_label(input_text, model)
-        df['similarity'] = df['Embed_sentence'].apply(lambda x: cosine_similarity(input_embedding_vector, x))
+        qVectorTFIDF, qVector = query(input_text)
+        simTFIDF = sgIndexWithTFIDF[qVectorTFIDF]
+
+        df['similarity_tfidf'] = simTFIDF
+        df_idf = df.sort_values(by='similarity_tfidf', ascending=False)
 
         pd.set_option('display.max_colwidth', None)
-        df_new = df.sort_values(by='similarity', ascending=False)
-        columns = ['Brand', 'Review Model', 'Review rating', 'Review Content']
-        df_select = df_new[columns]
+        columns = ['Brand', 'Review Model', 'Review rating', 'Verified Purchase or not', 
+                   'list price',  'Original title', 'Original review']
+        df_select = df_idf[columns]
         df_final = df_select[(df_select['Brand'] == brand) & (df_select['Review rating'] == rating)].head(4).reset_index(drop=True)
         df_final = df_final.drop_duplicates()
+        df_final.index += 1
         st.dataframe(df_final)
